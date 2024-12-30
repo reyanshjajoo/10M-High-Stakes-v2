42d9e5484bfc857168ddfbaf10c6b6c8c436779d
v0ic3less <reyanshja@outlook.com>
2024-12-30 13:40:25 +1300

    init motors/sensors

---
+++ 10M-High-Stakes-v2/src/constants.hpp
@@ -0,0 +1,28 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+// Motor ports
+const int LEFT_MOTOR_1 = 1;
+const int LEFT_MOTOR_2 = 2;
+const int LEFT_MOTOR_3 = 3;
+
+const int RIGHT_MOTOR_1 = 4;
+const int RIGHT_MOTOR_2 = 5;
+const int RIGHT_MOTOR_3 = 6;
+
+const int INTAKE_PORT = 7;
+const int HOOK_PORT = 8;
+const int LB_PORT = 9;
+
+// Sensor ports
+const int IMU_PORT = 10;
+const int LB_ROTATION_PORT = 11;
+const int HORTIZONTAL_TRACKING_ROTATION_PORT = 12;
+const int VERTICAL_TRACKING_ROTATION_PORT = 13;
+const int OPTICAL_PORT = 14;
+
+// Pneumatic ports
+const char CLAMP_PORT = 'A';
+const char DOINKER_PORT = 'B';
+
+#endif
